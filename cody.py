5d9a6d17bebe4baa6be04f58cfe661fd7bea2d5b
Chigoziee <61709944+Chigoziee@users.noreply.github.com>
2024-02-29 19:36:46 +0100

    Update cody.py

--- cody.py
+++ cody.py
@@ -1,24 +1,31 @@
-# Import the openai package
+# Import package
 from openai import OpenAI
+import streamlit as st
+
+
+st.header('CODY')
+st.subheader("Your python code Generator")
+with st.chat_message("Cody", avatar = '🤖'):
+    st.write("What code would you like to generate?")
 
 # initializing OpenAI Client
 client = OpenAI(api_key=API_KEY)
 
-print("###################### Hi, I'm cody a python code Generator ######################")
-
-rerun = 'y'
-while rerun == 'y':
-    # Getting user prompt
-    prompt = input('\n\nWhat code would you like me to generate:\n')
-    # Creating chatbot integration
-    cody = client.chat.completions.create(
-        model="gpt-3.5-turbo-1106",
-        messages=[{"role": "system", "content": """you are a code generation chatbot named Cody. You generate code
-                 in python and after that explain each line of the code and any ambiguous term in very simple
-                 english to an absolute beginner. You MUST ONLY do code generation and explanation task"""},
-                  {"role": "user", "content": prompt}
-                  ])
-    # Print response
-    print(cody.choices[0].message.content)
-    # This asks users if they want to perform another task
-    rerun = input('\n\nDo you want me to generate another code? enter (y) or any other key to exit: ')
+response = ''
+# Getting User input
+prompt = st.chat_input("Say something")
+if prompt:
+    with st.chat_message("User", avatar='👽'):
+        st.write(f"{prompt}")
+        # Creating chatbot integration
+        cody = client.chat.completions.create(
+            model="gpt-3.5-turbo-1106",
+            messages=[{"role": "system", "content": """you are a code generation chatbot named Cody. You generate code
+                             in python and after that explain each line of the code and any ambiguous term in very simple
+                             english to an absolute beginner. You MUST ONLY do code generation and explanation task"""},
+                              {"role": "user", "content": prompt}
+                              ])
+        response = cody.choices[0].message.content
+if aprompt:
+    with st.chat_message("Cody", avatar='🤖'):
+        st.write(response)
